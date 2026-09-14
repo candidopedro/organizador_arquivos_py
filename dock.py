498b52c4023deed74610069918e3206f71691cd5
candidopedro <pedro.s.candido@gmail.com>
2026-09-14 17:47:50 -0300

    limitando do item 1-5 - Bug de duas dock's

--- dock.py
+++ dock.py
@@ -36,22 +36,13 @@ class Interface:
 
 def barra_de_opcoes(self): #Navegar pela seta <- / -> e selecionar com ENTE - Fazer uma list
 
-    dock = {1:'[1] NOME', 2:'[2] TAMANHO', 3: '[3] DATA DE CRIAÇÃO', 4:'[4] DATA DE MODIFICAÇÃO', 5: '[5] TIPO DE ARQUIVO'}
     # {Back.BLUE}[1] NOME {Style.RESET_ALL} - adionar ao conjunto de caracteres no inicio e no fim quando selecionado - sempre atualizar a dock e a organização conforme for selecionando os tipos diferentes de organização
-    # print(f'│{4 * " "}[1] NOME │ [2] TAMANHO │ [3] DATA DE CRIAÇÃO │ [4] DATA DE MODIFICAÇÃO │ [5] TIPO DE ARQUIVO{4 * " "}│')
-    # command = int(input('<- or ->')) # Encontrar um forma de ler outras teclas como entrada
     
     item_dock = 1
     while True:
-        print('Pressione <- ou ->')
-        tecla = keyboard.read_key()
-        print(tecla)
-        if tecla == 'right' and item_dock < 6:
-            item_dock += 1
-        elif tecla == 'left' and item_dock > 1:
-            item_dock -= 1
+        
+        dock = {1:f'[1] NOME{Style.RESET_ALL}', 2:f'[2] TAMANHO{Style.RESET_ALL}', 3: f'[3] DATA DE CRIAÇÃO{Style.RESET_ALL}', 4:f'[4] DATA DE MODIFICAÇÃO{Style.RESET_ALL}', 5: f'[5] TIPO DE ARQUIVO{Style.RESET_ALL}'}
 
-        print(item_dock)
         for key, value in dock.items():
             if key == item_dock:
                 dock.update({item_dock: f"{Back.BLUE}{value}{Style.RESET_ALL}"})
@@ -64,8 +55,16 @@ def barra_de_opcoes(self): #Navegar pela seta <- / -> e selecionar com ENTE - Fa
                 print(f'{valor}    │')
             else:
                 print(f'{valor} │', end= " ")
-
         Interface.part_inferior(self, None)
-        break
+
+        print('Pressione <- ou ->')
+        tecla = keyboard.read_key()
+
+        if tecla == 'right' and item_dock < 5:
+            item_dock += 1
+        elif tecla == 'left' and item_dock > 1:
+            item_dock -= 1
+        print(tecla)
+
         
 barra_de_opcoes(None)
